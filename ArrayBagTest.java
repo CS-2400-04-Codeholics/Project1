90149d456d2417162d4933bda03af1ed864f4a4f
Jonathan Marquez <45045020+AshianBoii@users.noreply.github.com>
2022-02-18 12:29:34 -0800

    Add files via upload

---
+++ ArrayBagTest.java
@@ -0,0 +1,16 @@
+
+public interface BagInterface < T >
+{
+public BagInterface < T > union(BagInterface < T > Bag2);
+/* a new collection of the entries that occur in both collections
+Bag2 will have its contents compared to this bag
+returns a new bag containing the entries of this bag and Bag2 they might have multiple copies of same entry
+*/
+    }
+public class ArrayBagTest {  
+      public static void main(String[] args) {
+    
+    
+    }
+
+}
